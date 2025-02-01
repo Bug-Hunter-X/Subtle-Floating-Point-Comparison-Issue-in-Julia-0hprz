6a30c2f232d568754aa14c4578816ca248d01e79
BugHunter-X <askzenai@gmail.com>
2025-02-01 05:06:04 +0100

    Add bugSolution.jl file

---
+++ bugSolution.jl
@@ -0,0 +1,16 @@
+```julia
+function myfunction(x)
+  if x > 0
+    return x^2
+  elseif x ≈ 0  # Use ≈ for approximate comparison
+    return 0
+  else
+    return -x^2
+  end
+end
+
+println(myfunction(2))
+println(myfunction(0))
+println(myfunction(-2))
+println(myfunction(1e-16))
+```
